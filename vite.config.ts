af4c025af102ce6da839e90e9a85927d8a7da0a7
Luiz Gustavo <gusstavopdm@gmail.com>
2024-03-14 17:50:12 -0300

    feat(vite.config.ts): add test configuration to vite.config.ts to enable global variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+  },
   plugins: [tsconfigPaths()],
 })
