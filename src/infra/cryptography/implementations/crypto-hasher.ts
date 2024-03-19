62fc91942984e06249f8e952f49034bb7a5b7425
Luiz Gustavo <gusstavopdm@gmail.com>
2024-03-19 15:44:39 -0300

    feat(crypto-hasher.ts): implement CryptoHasher class to provide hash generation and comparison

--- src/infra/cryptography/implementations/crypto-hasher.ts
+++ src/infra/cryptography/implementations/crypto-hasher.ts
@@ -2,9 +2,16 @@ import crypto from 'node:crypto'
 
 import { env } from '@/infra/config/env'
 
+import { HashComparer } from '../hash-comparer'
 import { HashGenerator } from '../hash-generator'
 
-export class CryptoHashGenerator implements HashGenerator {
+export class CryptoHasher implements HashGenerator, HashComparer {
+  async compare(plain: string, hash: string): Promise<boolean> {
+    const hashed = await this.hash(plain)
+
+    return hashed === hash
+  }
+
   async hash(plain: string) {
     const hash = crypto.createHmac('sha256', env.SALT).update(plain)
 
