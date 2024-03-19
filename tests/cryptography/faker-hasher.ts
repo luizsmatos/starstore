68393757052745d5d94f03834c3a7e2995d8c521
Luiz Gustavo <gusstavopdm@gmail.com>
2024-03-19 15:44:58 -0300

    feat(faker-hasher.ts): implement HashComparer interface in FakerHasher class to provide comparison functionality for hashed values

--- tests/cryptography/faker-hasher.ts
+++ tests/cryptography/faker-hasher.ts
@@ -1,6 +1,13 @@
+import { HashComparer } from '@/infra/cryptography/hash-comparer'
 import { HashGenerator } from '@/infra/cryptography/hash-generator'
 
-export class FakerHasher implements HashGenerator {
+export class FakerHasher implements HashGenerator, HashComparer {
+  async compare(plain: string, hash: string): Promise<boolean> {
+    const hashed = await this.hash(plain)
+
+    return hashed === hash
+  }
+
   async hash(plain: string): Promise<string> {
     return plain.concat('-hashed')
   }
