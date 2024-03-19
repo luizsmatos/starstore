5e9b4905c80d57a447b40a8d9a5afd1e0aeacf3f
Luiz Gustavo <gusstavopdm@gmail.com>
2024-03-19 17:17:16 -0300

    feat(tests): add findAll method to InMemoryProductsRepository to retrieve all products from the repository

--- tests/repositories/in-memory-products-repository.ts
+++ tests/repositories/in-memory-products-repository.ts
@@ -4,6 +4,10 @@ import { ProductsRepository } from '@/domain/products/repositories/products-repo
 export class InMemoryProductsRepository implements ProductsRepository {
   public items: Product[] = []
 
+  async findAll(): Promise<Product[]> {
+    return this.items
+  }
+
   async create(product: Product): Promise<void> {
     this.items.push(product)
   }
